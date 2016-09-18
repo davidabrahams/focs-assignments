8fa91c788276d4571f68fd59ce3d22b569ea52c0
David Abrahams <davidabrahams@sbcglobal.net>
2016-09-18 11:21:41 -0400

    Copy pasta code

--- day5/hw4.rkt
+++ day5/hw4.rkt
@@ -1,8 +1,33 @@
 #lang racket
 
-;;; Student Name: Frankly Olin [change to your name]
+;;; Student Name: David Abrahams
 ;;;
 ;;; Check one:
 ;;; [ ] I completed this assignment without assistance or external resources.
-;;; [ ] I completed this assignment with assistance from ___
-;;;     and/or using these external resources: ___
+;;; [x] I completed this assignment with assistance from Annabel lConsilvio. We worked together!
+
+(define (print-square x)
+  (display "the square of ")
+  (display x)
+  (display " is ")
+  (display (* x x)))
+
+(display "enter a number")
+(print-square (read))
+
+(define (run-repl)
+  (display "welcome to my repl.  type some scheme-ish")
+  (repl))
+
+(define (repl)
+  (display "> ")
+  (display (myeval (read)))
+  (newline)
+  (repl))
+
+(define (myeval sexpr)
+  sexpr)
+
+;; Run this by typing (run-repl)  at scheme.
+;; Note that you do not need to quote what you type in; 
+;;   it is read as though it were quoted already.
